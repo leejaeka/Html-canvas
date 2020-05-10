b0b2f83ef656f8d65ac01cf2f592958b848faf7d
Jaekang Lee <jaekang.lee@mail.utoronto.ca>
2020-05-10 02:50:25 -0700

    fini!
    yay

--- script.js
+++ script.js
@@ -77,38 +77,110 @@ const centerY = canvas.height / 2;
 //ctx.strokeStyle = 'orange';
 //ctx.strokeText('Hello World', 400, 100);
 
-//Animation 1
-const circle = {
-    x:200, y : 200, size:30, dx:5,dy:4
+////Animation 1
+//const circle = {
+//    x:200, y : 200, size:30, dx:5,dy:4
+//}
+//function drawCircle() {
+//    ctx.beginPath();
+//    ctx.arc(circle.x, circle.y, circle.size, 0, Math.PI * 2);
+//    ctx.fillStyle = 'purple';
+//    ctx.fill();
+//}
+//function update() {
+//    ctx.clearRect(0, 0, canvas.width, canvas.height);
+//    drawCircle();
+//    //change position
+//    circle.x += circle.dx;
+//    circle.y += circle.dy;
+//    requestAnimationFrame(update);
+
+//    //detect side walls
+//    if (circle.x + circle.size > canvas.width || circle.x - circle.size < 0) {
+//        circle.dx *= -1;
+
+//    }
+//    // detect top/bottom walls
+//    if (circle.y + circle.size > canvas.height || circle.y - circle.size < 0) {
+//        circle.dy *= -1;
+
+//    }
+//}
+//update();
+
+//Animation 2
+const image = document.getElementById('source');
+const player = {
+    w: 50, h: 70, x: 20, y: 200, speed: 5, dx: 0, dy: 0
+};
+function drawPlayer() {
+    ctx.drawImage(image, player.x, player.y, player.w, player.h);
 }
-function drawCircle() {
-    ctx.beginPath();
-    ctx.arc(circle.x, circle.y, circle.size, 0, Math.PI * 2);
-    ctx.fillStyle = 'purple';
-    ctx.fill();
+function clear() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+function newPos() {
+    player.x += player.dx;
+    player.y += player.dy;
+    detectWall();
+}
+function detectWall() {
+    if (player.x < 0) {
+        player.x = 0;
+    }
+    if (player.x + player.w > canvas.width) {
+        player.x = canvas.width - player.w;
+    }
+    if (player.y < 0) {
+        player.y = 0;
+    }
+    if (player.y + player.h > canvas.height) {
+        player.y = canvas.height - player.h;
+    }
 }
 function update() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawCircle();
-    //change position
-    circle.x += circle.dx;
-    circle.y += circle.dy;
+    clear();
+    drawPlayer();
+    newPos();
     requestAnimationFrame(update);
-
-    //detect side walls
-    if (circle.x + circle.size > canvas.width || circle.x - circle.size < 0) {
-        circle.dx *= -1;
-
+}
+function moveUp() {
+    player.dy = -player.speed;
+    console.log('??');
+}
+function moveDown() {
+    player.dy = player.speed;
+}
+function moveRight() {
+    player.dx = player.speed;
+}
+function moveLeft() {
+    player.dx = -player.speed;
+}
+function keyDown(e) {
+    if (e.key == 'ArrowRight' || e.key == 'Right') {
+        moveRight();
     }
-    // detect top/bottom walls
-    if (circle.y + circle.size > canvas.height || circle.y - circle.size < 0) {
-        circle.dy *= -1;
-
+    else if (e.key == 'ArrowLeft' || e.key == 'Left') {
+        moveLeft();
+    }
+    else if (e.key == 'ArrowUp' || e.key == 'Up') {
+        moveUp();
+    }
+    else if (e.key == 'ArrowDown' || e.key == 'Down') {
+        moveDown();
+    }
+    
+}
+function keyUp(e) {
+    if (e.key == 'ArrowRight' || e.key == 'Right' ||
+        e.key == 'ArrowLeft' || e.key == 'Left' ||
+        e.key == 'ArrowUp' || e.key == 'Up' ||
+        e.key == 'ArrowDown' || e.key == 'Down') {
+        player.dx -= 1;
+        player.dy -= 1;
     }
 }
 update();
-
-//Animation 2
-const player = {
-    w: 50, h: 70, x: 20, y: 200, speed: 5, dx: 0, dy: 0
-}
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
