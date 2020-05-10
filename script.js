d49640745248e358bb4d255ccde8562cd1369ef2
Jaekang Lee <jaekang.lee@mail.utoronto.ca>
2020-05-09 23:31:17 -0700

    lol
    lol

---
+++ script.js
@@ -0,0 +1,114 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const centerX = canvas.width / 2;
+const centerY = canvas.height / 2;
+
+////Triangle
+//ctx.beginPath();
+//ctx.moveTo(50, 50);
+//ctx.lineTo(150, 50);
+//ctx.stroke();
+//ctx.lineTo(100, 200);
+//ctx.closePath();
+//ctx.fillStyle = 'coral';
+//ctx.fill();
+
+//ctx.beginPath();
+//ctx.moveTo(200, 50);
+//ctx.lineTo(150, 200);
+//ctx.lineTo(250, 200);
+//ctx.closePath();
+//ctx.fillStyle = 'teal';
+//ctx.fill();
+//ctx.stroke(); //OUTLINES
+
+////Rectangle
+//ctx.beginPath();
+//ctx.rect(300, 50, 150, 100);
+//ctx.fillStyle = 'red';
+//ctx.fill();
+
+////ARC (CIRCLE) draw head
+//ctx.beginPath();
+//ctx.arc(canvas.width/2, canvas.height/2, 200, 0, Math.PI * 2);
+////mouth
+//ctx.moveTo(canvas.width / 2 + 100, centerY);
+//ctx.arc(centerX, centerY, 100, 0, Math.PI, false); 
+
+////Move left eye
+//ctx.moveTo(centerX - 60, centerY - 80);
+//ctx.arc(centerX - 80, centerY - 80, 20, 0, Math.PI * 2);
+//ctx.moveTo(centerY-80,centerY-80)
+
+//ctx.stroke();
+
+//// Quadratric curves example
+//ctx.beginPath();
+//ctx.moveTo(75, 25);
+//ctx.quadraticCurveTo(25, 25, 25, 62.5);
+//ctx.quadraticCurveTo(25, 100, 50, 100);
+//ctx.quadraticCurveTo(50, 120, 30, 125);
+//ctx.quadraticCurveTo(60, 120, 65, 100);
+//ctx.quadraticCurveTo(125, 100, 125, 62.5);
+//ctx.quadraticCurveTo(125, 25, 75, 25);
+//ctx.stroke();
+
+////canvas.width = 200;
+////fillRect()
+//ctx.fillStyle = 'red';
+//ctx.fillRect(20,20,150,100);
+//ctx.fillRect(200,20,150,100);
+
+////strokeRect()
+//ctx.lineWidth = 5;
+//ctx.strokeStyle = 'green';
+//ctx.strokeRect(100, 200, 150, 100);
+
+////clearRect()
+//ctx.clearRect(25, 25, 140, 90);
+
+////fillText()
+//ctx.font = '30px Arial';
+//ctx.fillStyle = 'purple';
+//ctx.fillText('Hello World', 400, 50);
+
+////strokeText()
+//ctx.lineWidth = 1;
+//ctx.strokeStyle = 'orange';
+//ctx.strokeText('Hello World', 400, 100);
+
+//Animation 1
+const circle = {
+    x:200, y : 200, size:30, dx:5,dy:4
+}
+function drawCircle() {
+    ctx.beginPath();
+    ctx.arc(circle.x, circle.y, circle.size, 0, Math.PI * 2);
+    ctx.fillStyle = 'purple';
+    ctx.fill();
+}
+function update() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawCircle();
+    //change position
+    circle.x += circle.dx;
+    circle.y += circle.dy;
+    requestAnimationFrame(update);
+
+    //detect side walls
+    if (circle.x + circle.size > canvas.width || circle.x - circle.size < 0) {
+        circle.dx *= -1;
+
+    }
+    // detect top/bottom walls
+    if (circle.y + circle.size > canvas.height || circle.y - circle.size < 0) {
+        circle.dy *= -1;
+
+    }
+}
+update();
+
+//Animation 2
+const player = {
+    w: 50, h: 70, x: 20, y: 200, speed: 5, dx: 0, dy: 0
+}
